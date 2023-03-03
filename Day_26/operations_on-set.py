fd7700f2982ebc9f961d3c78b698564b3fac8594
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-03-04 00:31:25 +0530

    added files

--- Day_26/operations_on-set.py
+++ Day_26/operations_on-set.py
@@ -31,4 +31,53 @@ print(s1.difference(s2))
 
 
 
-#
+## SET METHODS 
+
+# isdisjoint()
+print(s1.isdisjoint(s2))
+
+
+# issuperset()
+print(s1.issuperset(s2))
+
+
+# issubset()
+print(s1.issubset(s2))
+
+# add a single item to the set
+s1.add(57)
+print(s1)
+
+
+#update()
+# If you want to add more than one item, simply create another set or any other iterable object(list, tuple, dictionary), and use the update() method to add it into the existing set.
+
+s1.update(s2)
+print(s1)
+
+
+# remove() - used to remove an item form list
+#The main difference between remove and discard is that, if we try to delete an item which is not present in set, then remove() raises an error, whereas discard() does not raise any error.
+
+s1.remove(6)
+print(s1)
+
+
+# pop() - """
+# The main difference between remove and discard is that, 
+# if we try to delete an item which is not present in set, 
+# then remove() raises an error, whereas discard() does not raise any error.
+
+
+s1.pop()
+print(s1)
+
+
+# del is used to delete the set entirely
+
+del s1
+
+
+# clear() method is used to clears all items in the set and prints and empty set
+s2.clear()
+print(s2)
